c8cfb403464dfc727e2ac3bc6d05f78c72442c68
Мухаммад Сабалиров <60626980+MikelazDotPy@users.noreply.github.com>
2024-04-20 16:50:20 +0300

    Add files via upload

--- db/uwu_data_base.py
+++ db/uwu_data_base.py
@@ -1,5 +1,6 @@
 import sqlalchemy as db
 from sqlalchemy import Table, Column, Integer, String, Text
+import csv
 engine = db.create_engine('sqlite:///uwu.db')
 conn = engine.connect()
 
@@ -7,6 +8,7 @@ metadata = db.MetaData()
 #Title,SpecialtyId,DepartmentId,StudyFormId,StudyLevelId,StudyPeriod,FakultetID
 Special = Table('special', metadata,
                 Column('id', Integer, primary_key=True),
+                Column('plan_id', Integer),
                 Column('name', Text),
                 Column('specialty_id', Integer),
                 Column('department_id', Integer),
@@ -23,3 +25,20 @@ def getSpecialties(fakultet_id: int):
 
 if __name__ == '__main__':
     metadata.create_all(engine)
+    #Title,SpecialtyId,DepartmentId,StudyFormId,StudyLevelId,StudyPeriod,FakultetId
+    with open('destination 2 (2).csv', 'r') as f:
+        rows = csv.reader(f, delimiter=',')
+        ff = False
+        for row in rows:
+            if ff:
+                insertion = Special.insert().values(plan_id=row[0],name = row[1], specialty_id = int(row[2]),
+                                                    department_id = int(row[3]),
+                                                    fakultet_id = int(row[7]),
+                                                    study_period = int(row[6]),
+                                                    study_form_id = int(row[5]),
+                                                    study_level_id = int(row[4]))
+                conn.execute(insertion)
+            
+            ff = True
+
+    conn.commit()
