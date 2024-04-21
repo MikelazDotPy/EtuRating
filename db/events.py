f7f584236816717cab6061159e06cb88fe8d0b9a
Gorsky Kirill <kirillgorskiy@gmail.com>
2024-04-21 08:04:17 +0300

    :)

--- db/events.py
+++ db/events.py
@@ -1,6 +1,8 @@
 import db.uwuconfig as cfg
 import requests as r
 import json
+import db.uwu_data_base as db
+from fuzzywuzzy import process
 
 class Event:
     def __init__(self, eventFormat: str = ""):
@@ -25,7 +27,17 @@ def getRNFList():
 def getRNF(id):
     return json.loads(r.get(cfg.spb_pnvsh_url + f'/api/v1/public/rnf/{id}').text)
 
+rnf = getRNFList()
+
+def getSuitableRNFs(session, plan_id):
+    q = session.query(db.EtuStrPlan.subject).filter(db.EtuStrPlan.plan_id == plan_id).all()
+    res = []
+    for x in q:
+        arr = list(map(lambda x: x[0], process.extract(x[0], rnf, limit=3)))
+        arr += res
+    return res
+
 if __name__ == "__main__":
     #print(getEvents(""))
     print(getRNFList())
-    #print(getRNF(5262))
+    
